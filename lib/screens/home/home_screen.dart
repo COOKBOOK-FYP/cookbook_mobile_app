54ee0d712874301094b7f9ea1f038682565ebe04
Wasim <wasimxaman13@gmail.com>
2023-06-21 15:48:28 +0500

    primary appbar created

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,27 @@
+import 'package:cookbook/widgets/appbar/primary_appbar_widget.dart';
+import 'package:cookbook/widgets/page/page_widget.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const PrimaryAppbarWidget(),
+      body: PageWidget(
+        children: [],
+      ),
+    );
+  }
+}
